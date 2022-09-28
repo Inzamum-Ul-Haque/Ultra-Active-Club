f0920b64b385e9d0113026a6b86666fc38bf63cc
Inzamum-Ul-Haque <inzamum.inu@gmail.com>
2022-09-29 01:51:05 +0600

    card component created and data shown

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Excercises from "../Excercises/Excercises";
 import Details from "../Details/Details";
 import "./Container.css";
 
 const Container = () => {
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    fetch("fakeDb.json")
+      .then((res) => res.json())
+      .then((data) => setCards(data));
+  }, []);
+
   return (
     <div className="app-container">
-      <div className="excercise-container">
+      <div className="header">
         <h2>Bangladesh Gym Club</h2>
-        <div className="card-container">
+        <div className="excercise-container">
           <h4>Select Excercise List</h4>
+          <div className="card-container">
+            {cards.map((card) => (
+              <Excercises key={card.id} card={card} />
+            ))}
+          </div>
         </div>
       </div>
       <Details />
--- src/components/Excercises/Excercises.js
+++ src/components/Excercises/Excercises.js
@@ -1,8 +1,22 @@
 import React from "react";
 import "./Excercises.css";
 
-const Excercises = () => {
-  return <div></div>;
+const Excercises = ({ card }) => {
+  const { image, heading, desc, age, time } = card;
+  return (
+    <div className="card">
+      <img src={image} alt="" />
+      <h3>{heading}</h3>
+      <p className="desc">{desc}</p>
+      <p>
+        For age: <strong>{age}</strong>
+      </p>
+      <p>
+        Time Required: <strong>{time}</strong>s
+      </p>
+      <button>Add to list</button>
+    </div>
+  );
 };
 
 export default Excercises;
